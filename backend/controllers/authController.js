23bb0c7954433081cf1f7a81088f9a7a3d257518
huydt7702 <developer.vanhuy@gmail.com>
2023-03-20 08:46:52 +0700

    fix some bug :)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -53,18 +53,20 @@ const authController = {
         try {
             const user = await User.findOne({ username: req.body.username });
             if (!user) {
-                res.status(404).json("Incorrect username");
+                return res.status(404).json("Incorrect username");
             }
             const validPassword = await bcrypt.compare(req.body.password, user.password);
             if (!validPassword) {
-                res.status(404).json("Incorrect password");
+                return res.status(404).json("Incorrect password");
             }
             if (user && validPassword) {
                 //Generate access token
                 const accessToken = authController.generateAccessToken(user);
+
                 //Generate refresh token
                 const refreshToken = authController.generateRefreshToken(user);
                 refreshTokens.push(refreshToken);
+
                 //STORE REFRESH TOKEN IN COOKIE
                 res.cookie("refreshToken", refreshToken, {
                     httpOnly: true,
@@ -72,6 +74,7 @@ const authController = {
                     path: "/",
                     sameSite: "strict",
                 });
+
                 const { password, ...others } = user._doc;
                 res.status(200).json({ ...others, accessToken, refreshToken });
             }
@@ -83,6 +86,7 @@ const authController = {
     requestRefreshToken: async (req, res) => {
         //Take refresh token from user
         const refreshToken = req.cookies.refreshToken;
+
         //Send error if token is not valid
         if (!refreshToken) return res.status(401).json("You're not authenticated");
         if (!refreshTokens.includes(refreshToken)) {
@@ -97,7 +101,7 @@ const authController = {
             const newAccessToken = authController.generateAccessToken(user);
             const newRefreshToken = authController.generateRefreshToken(user);
             refreshTokens.push(newRefreshToken);
-            res.cookie("refreshToken", refreshToken, {
+            res.cookie("refreshToken", newRefreshToken, {
                 httpOnly: true,
                 secure: false,
                 path: "/",
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,8 @@
 const userController = require("../controllers/userController");
-const {
-  verifyToken,
-  verifyTokenAndAdmin,
-  verifyTokenAndUserAuthorization,
-} = require("../controllers/verifyToken");
+const { verifyToken, verifyTokenAndAdmin, verifyTokenAndUserAuthorization } = require("../controllers/verifyToken");
 
 const router = require("express").Router();
+
 //GET ALL USERS
 router.get("/", verifyToken, userController.getAllUsers);
 
