e6cf29b5bd183b78713b7ed46a92a133b89b967f
huydt7702 <developer.vanhuy@gmail.com>
2023-03-21 08:01:49 +0700

    add background-image for login and register page

--- frontend/src/pages/client/Auth/Auth.js
+++ frontend/src/pages/client/Auth/Auth.js
@@ -40,84 +40,91 @@ function Auth() {
     };
 
     return (
-        <div className={cx('container', changeLayout ? 'right-panel-active' : '')}>
-            <div className={cx('form-container', 'sign-up-container')}>
-                <form onSubmit={handleRegister}>
-                    <h1>Create Account</h1>
-                    <div className={cx('social-container')}>
-                        <a href="/" className={cx('social')}>
-                            <FacebookIcon />
-                        </a>
-                        <a href="/" className={cx('social')}>
-                            <GooglePlusIcon />
-                        </a>
-                        <a href="/" className={cx('social')}>
-                            <LinkedInIcon />
-                        </a>
-                    </div>
-                    <span>or use your email for registration</span>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <button>Sign Up</button>
-                </form>
-            </div>
-            <div className={cx('form-container', 'sign-in-container')}>
-                <form onSubmit={handleLogin}>
-                    <h1>Sign in</h1>
-                    <div className={cx('social-container')}>
-                        <a href="/" className={cx('social')}>
-                            <FacebookIcon />
-                        </a>
-                        <a href="/" className={cx('social')}>
-                            <GooglePlusIcon />
-                        </a>
-                        <a href="/" className={cx('social')}>
-                            <LinkedInIcon />
-                        </a>
-                    </div>
-                    <span>or use your account</span>
-                    <input
-                        type="text"
-                        placeholder="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
-                    <input
-                        type="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                    <a href="/">Forgot your password?</a>
-                    <button>Sign In</button>
-                </form>
-            </div>
-            <div className={cx('overlay-container')}>
-                <div className={cx('overlay')}>
-                    <div className={cx('overlay-panel', 'overlay-left')}>
-                        <h1>Welcome Back!</h1>
-                        <p>To keep connected with us please login with your personal info</p>
-                        <button className={cx('ghost')} onClick={() => setChangeLayout(false)}>
-                            Sign In
-                        </button>
-                    </div>
-                    <div className={cx('overlay-panel', 'overlay-right')}>
-                        <h1>Hello, Friend!</h1>
-                        <p>Enter your personal details and start journey with us</p>
-                        <button className={cx('ghost')} onClick={() => setChangeLayout(true)}>
-                            Sign Up
-                        </button>
+        <div className={cx('wrapper')}>
+            <div className={cx('container', changeLayout ? 'right-panel-active' : '')}>
+                <div className={cx('form-container', 'sign-up-container')}>
+                    <form onSubmit={handleRegister}>
+                        <h1>Create Account</h1>
+                        <div className={cx('social-container')}>
+                            <a href="/" className={cx('social')}>
+                                <FacebookIcon />
+                            </a>
+                            <a href="/" className={cx('social')}>
+                                <GooglePlusIcon />
+                            </a>
+                            <a href="/" className={cx('social')}>
+                                <LinkedInIcon />
+                            </a>
+                        </div>
+                        <span>or use your email for registration</span>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button>Sign Up</button>
+                    </form>
+                </div>
+                <div className={cx('form-container', 'sign-in-container')}>
+                    <form onSubmit={handleLogin}>
+                        <h1>Sign in</h1>
+                        <div className={cx('social-container')}>
+                            <a href="/" className={cx('social')}>
+                                <FacebookIcon />
+                            </a>
+                            <a href="/" className={cx('social')}>
+                                <GooglePlusIcon />
+                            </a>
+                            <a href="/" className={cx('social')}>
+                                <LinkedInIcon />
+                            </a>
+                        </div>
+                        <span>or use your account</span>
+                        <input
+                            type="text"
+                            placeholder="Username"
+                            value={username}
+                            onChange={(e) => setUsername(e.target.value)}
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <a href="/">Forgot your password?</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+                <div className={cx('overlay-container')}>
+                    <div className={cx('overlay')}>
+                        <div className={cx('overlay-panel', 'overlay-left')}>
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button className={cx('ghost')} onClick={() => setChangeLayout(false)}>
+                                Sign In
+                            </button>
+                        </div>
+                        <div className={cx('overlay-panel', 'overlay-right')}>
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className={cx('ghost')} onClick={() => setChangeLayout(true)}>
+                                Sign Up
+                            </button>
+                        </div>
                     </div>
                 </div>
             </div>
